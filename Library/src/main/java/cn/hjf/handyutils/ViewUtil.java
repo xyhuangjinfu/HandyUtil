7f551bd8f2b8736c5afc699f18cd38a1cb6e810e
huangjinfu <huangjinfu@1949hk.com>
2017-07-12 16:17:20 +0800

    添加设置margin功能

--- Library/src/main/java/cn/hjf/handyutils/ViewUtil.java
+++ Library/src/main/java/cn/hjf/handyutils/ViewUtil.java
@@ -29,4 +29,27 @@ public final class ViewUtil {
 
         view.setLayoutParams(lp);
     }
+
+    /**
+     * 设置 View 的 margin
+     *
+     * @param view
+     * @param left   null-保持原有的 margin
+     * @param top
+     * @param right
+     * @param bottom
+     */
+    public static void setMargin(View view, Integer left, Integer top, Integer right, Integer bottom) {
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+        if (lp != null && lp instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) lp;
+            mlp.setMargins(
+                    left == null ? mlp.leftMargin : left,
+                    top == null ? mlp.topMargin : top,
+                    right == null ? mlp.rightMargin : right,
+                    bottom == null ? mlp.bottomMargin : bottom
+            );
+            view.setLayoutParams(mlp);
+        }
+    }
 }
